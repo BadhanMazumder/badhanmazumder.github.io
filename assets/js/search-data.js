03ffa8fa8ef27953fafc008ad03bef9bb3b0146d
BadhanMazumder <badhanmazumdar18@gmail.com>
2026-02-05 03:50:44 +0000

    Deploying to gh-pages from @ BadhanMazumder/badhanmazumder.github.io@271b63f03d3dbc03415e8d5cef1b1b075455595b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,10 +11,10 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cv/";
+            window.location.href = "/assets/pdf/example_pdf.pdf";
           },
         },{id: "post-a-post-with-plotly-js",
         
