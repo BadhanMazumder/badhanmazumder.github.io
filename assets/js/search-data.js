3b50fb85fd4c943ce13f23ab8aaceda37f8d6016
BadhanMazumder <badhanmazumdar18@gmail.com>
2026-06-16 01:52:33 +0000

    Deploying to gh-pages from @ BadhanMazumder/badhanmazumder.github.io@7135c9946f16bca5d0390e27731c27168e2716c7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,7 +384,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-our-work-on-kuramoto-guided-graph-learning-for-brain-structure-function-coupling-under-prenatal-drug-exposure-has-been-accepted-in-isbi-2026",
+            },},{id: "news-our-work-on-kuramoto-guided-graph-learning-for-brain-structure-function-coupling-under-prenatal-drug-exposure-has-been-accepted-to-isbi-2026",
           title: 'Our work on Kuramoto-guided graph learning for brain structure–function coupling under prenatal drug...',
           description: "",
           section: "News",},{id: "news-two-papers-on-longitudinal-brain-connectivity-and-graph-based-representation-learning-have-been-accepted-to-icassp-2026",
